64b9aa65a030c5a876a5e0faafecb09317741ae9
Chintya-aa <tyachintyachin@gmail.com>
2025-03-26 23:38:03 +0800

    penulisan step 1-5

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -37,3 +37,18 @@ void inserttionSort()
     int temp ; // membuat variabel data temporer atau menyimpan sementara
     int j, i;// membuat variabel j sebagai penanda
 
+    for(i=1;i<=n-1;i++)
+    {//step 1
+
+        temp=arr[i];//step 2
+        j=i-1;//step3
+        while (j>=0&& arr [j] > temp)// step 4
+        {
+            arr[j+1]=arr[j] ;//step 4a
+            j--;//step 4b
+        }
+
+        arr[j+1] = temp ; // step 5
+    }
+}
+
